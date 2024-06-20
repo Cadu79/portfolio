28bb64aae3f0966fe3d859ee4cf986ec405b0c94
Cadu 79 <eduardoprimogenito@gmail.com>
2024-06-20 17:33:35 -0300

    adicionando pasta containers e components

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import Titulo from "../../components/Titulo"
+
+/* eslint-disable jsx-a11y/alt-text */
+const Sidebar = () => (
+  <aside>
+      <img src="https://github.com/cadu79.png"/>
+      <Titulo>Carlos Eduardo</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,12 @@
+import Titulo from "../../components/Titulo"
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam ipsam rerum itaque, ab dolore incidunt expedita recusandae consequuntur est labore commodi debitis esse, maiores voluptatum corrupti. Sapiente aut tempore nesciunt.
+    </p>
+  </section>
+)
+
+export default Sobre
