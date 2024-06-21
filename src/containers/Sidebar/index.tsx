70a2a6be07aa4e73e5433744ce9b09f66e007b9c
Cadu 79 <eduardoprimogenito@gmail.com>
2024-06-21 11:54:47 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,19 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { Descricao, BotaoTema, SidebarContainer } from "./styles"
 
 /* eslint-disable jsx-a11y/alt-text */
 const Sidebar = () => (
   <aside>
-      <img src="https://github.com/cadu79.png"/>
-      <Titulo fontSize={20}>Carlos Eduardo</Titulo>
+    <SidebarContainer>
+        <Avatar />
+        <Titulo fontSize={20}>Carlos Eduardo</Titulo>
+        <Paragrafo tipo="secundario" fontSize={16}>Cadu79</Paragrafo>
+        <Descricao tipo="principal" fontSize={12}>Engenheiro front-end</Descricao>
+        <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
