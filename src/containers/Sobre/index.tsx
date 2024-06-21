620ae42f13071fa48ecc65cea3e49dfbe0b6bf8e
Cadu 79 <eduardoprimogenito@gmail.com>
2024-06-21 10:35:04 -0300

    concluindo secao sobre

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Titulo from "../../components/Titulo"
 import Paragrafo from "../../components/Paragrafo"
+import { GithubSecao } from "./styles"
 
 const Sobre = () => (
   <section>
@@ -7,6 +9,10 @@ const Sobre = () => (
     <Paragrafo tipo="principal">
     Carlos Eduardo da Silva, um dedicado professor de Matemática, está trilhando um novo caminho ao migrar para a área de tecnologia, focando tanto no desenvolvimento back end quanto no front end. Com uma sólida base analítica e uma paixão por resolver problemas, traz uma abordagem lógica e estruturada ao mundo do desenvolvimento de software. Sua transição para a tecnologia reflete um desejo contínuo de aprender e inovar, utilizando suas habilidades matemáticas para enfrentar novos desafios e contribuir significativamente para projetos de desenvolvimento web e de sistemas complexos.
     </Paragrafo>
+    <GithubSecao>
+    <img src="https://github-readme-stats.vercel.app/api?username=Cadu79&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Cadu79&layout=compact&langs_count=7&theme=dracula"/>
+    </GithubSecao>
   </section>
 )
 
