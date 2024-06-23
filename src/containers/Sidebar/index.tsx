9c4104c0f278b8c3d3727c4304af67d4b47d1940
Cadu 79 <eduardoprimogenito@gmail.com>
2024-06-23 18:53:25 -0300

    adicionando o theme

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -4,15 +4,19 @@ import Titulo from "../../components/Titulo"
 
 import { Descricao, BotaoTema, SidebarContainer } from "./styles"
 
+type Props = {
+  trocaTema: () => void;
+}
+
 /* eslint-disable jsx-a11y/alt-text */
-const Sidebar = () => (
+const Sidebar = (props: Props) => (
   <aside>
     <SidebarContainer>
         <Avatar />
         <Titulo fontSize={20}>Carlos Eduardo</Titulo>
         <Paragrafo tipo="secundario" fontSize={16}>Cadu79</Paragrafo>
         <Descricao tipo="principal" fontSize={12}>Engenheiro front-end</Descricao>
-        <BotaoTema>Trocar tema</BotaoTema>
+        <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
     </SidebarContainer>
   </aside>
 )
